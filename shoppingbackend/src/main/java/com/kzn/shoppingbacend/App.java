3db0ae6ca5151328fe038afe98ab2fa38ee1d54a
anish238 <41230691+anish238@users.noreply.github.com>
2018-08-01 18:20:50 +0530

    my project setup

---
+++ shoppingbackend/src/main/java/com/kzn/shoppingbacend/App.java
@@ -0,0 +1,13 @@
+package com.kzn.shoppingbacend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
